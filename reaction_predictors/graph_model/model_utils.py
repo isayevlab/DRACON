8a5a7b575cc774ad3a86c916508b7f2662f74df5
Philipp Nikitin <filipp.nikitin.96@gmail.com>
2019-11-27 04:12:22 +0300

    limited calculation of gradients during evaluation

--- reaction_predictors/graph_model/model_utils.py
+++ reaction_predictors/graph_model/model_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
+import torch
 
 from torch import nn
-from tqdm import tqdm
 from torch.nn.utils import clip_grad_norm_
 from sklearn.metrics import f1_score
 
@@ -9,7 +9,7 @@ from sklearn.metrics import f1_score
 def train_epoch(model, loader, optimizer, scheduler, clip_grad=1e-3):
     model.train()
     loss_list = []
-    for batch in tqdm(loader):
+    for batch in loader:
         graph, targets = batch[0], batch[1:]
         optimizer.zero_grad()
         loss = model.get_loss(graph, targets)
@@ -27,18 +27,18 @@ def test(model, loader):
     result = []
     for _ in range(model.n_model_heads):
         result.append({'target': [], 'predicted': []})
-
-    for batch in loader:
-        graph, targets = batch[0], batch[1:]
-        outputs = model(graph)
-        for i, (out, target) in enumerate(zip(outputs, targets)):
-            predicted = (sigmoid(out) > .5).float().cpu().detach().numpy()
-            target = target.float().cpu().detach().numpy()
-            for pred, tar in zip(predicted, target):
-                pred = pred[tar != -1]
-                tar = tar[tar != -1]
-                result[i]['target'].append(tar)
-                result[i]['predicted'].append(pred)
+    with torch.no_grad():
+        for batch in loader:
+            graph, targets = batch[0], batch[1:]
+            outputs = model(graph)
+            for i, (out, target) in enumerate(zip(outputs, targets)):
+                predicted = (sigmoid(out) > .5).float().cpu().detach().numpy()
+                target = target.float().cpu().detach().numpy()
+                for pred, tar in zip(predicted, target):
+                    pred = pred[tar != -1]
+                    tar = tar[tar != -1]
+                    result[i]['target'].append(tar)
+                    result[i]['predicted'].append(pred)
     return result
 
 
